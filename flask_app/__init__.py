8ad5d3c259c07e8fa1e146963fc2c338c3096c75
Joshua Wise <joshuawise@joshuas-mbp.myfiosgateway.com>
2023-02-01 15:24:07 -0500

    addded s3 creds

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,3 +3,7 @@ from os import environ
 
 application = Flask(__name__)
 application.secret_key = environ.get('SECRET_KEY')
+application.config["S3_SECRET_KEY"] = environ.get('S3_SECRET_KEY')
+application.config["S3_BUCKET"] = environ.get('S3_BUCKET')
+application.config["S3_KEY"] = environ.get('S3_KEY')
+application.config["S3_SECRET"] = environ.get('S3_SECRET')
